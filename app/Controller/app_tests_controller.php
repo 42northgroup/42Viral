dd06be9686ace01856e85c552ab5183aacb1a0cd
Jason Snider <jsnider77@gmail.com>
2011-07-04 14:49:04 -0700

    Test and Experiments tweaks
    Completed first working unit test
    Cleaned out the app_test controller

--- app/Controller/app_tests_controller.php
+++ app/Controller/app_tests_controller.php
@@ -7,14 +7,10 @@ class AppTestsController extends AppController {
     var $uses = array();
     
     public function index(){
-        $this->loadModel('Person');
-       // pr($this->Person->find('all'));
-        
-        pr($this->Person->getPerson('c9510606-a5bf-11e0-8563-000c29ae9eb4'));
-    }
+
+    }  
     
-    
-    
+
 }
 
 ?>
--- app/Test/Case/Model/PersonTest.php
+++ app/Test/Case/Model/PersonTest.php
@@ -10,8 +10,15 @@ class PersonTestCase extends CakeTestCase {
         $result = $this->Person->getPerson('c9510606-a5bf-11e0-8563-000c29ae9eb4');
 
         $expected = array(
-            array('Person' => 
-                array('first_name' => 'test' ))
+            'Person' => array(
+                'id' => 'c9510606-a5bf-11e0-8563-000c29ae9eb4',
+                'first_name' => 'test',
+                'last_name' => 'data',
+                'created' => '2011-07-03 00:00:00',
+                'created_person_id' => 'c9510606-a5bf-11e0-8563-000c29ae9eb4',
+                'modified' => '2011-07-03 00:00:00',
+                'modified_person_id' => 'c9510606-a5bf-11e0-8563-000c29ae9eb4'
+            )
         );
 
         $this->assertEqual($result, $expected);
