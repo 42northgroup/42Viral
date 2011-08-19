8ac40201551a62c5da8b748f2a7a955e65be28df
Jason Snider <jsnider77@gmail.com>
2011-08-19 05:58:10 -0700

    ScrubTest
    - Added test cases against Scrub::html()

--- app/Test/Case/Lib/ScrubTest.php
+++ app/Test/Case/Lib/ScrubTest.php
@@ -18,6 +18,33 @@ class ScrubTestCase extends CakeTestCase {
     }
     
     /**
+     * Tests that <p></p> is applied to nl2br scenarios
+     */
+    public function testHTMLAutoParagraph(){
+        $result = Scrub::html('Bob is cool');
+        $expected = '<p>Bob is cool</p>';
+        $this->assertEqual($result, $expected);
+    }
+    
+    /**
+     * Tests that <p></p> is applied to nl2br scenarios
+     */
+    public function testHTMLinkify(){
+        $result = Scrub::html('http://www.42viral.org');
+        $expected = '<p><a href="http://www.42viral.org">http://www.42viral.org</a></p>';
+        $this->assertEqual($result, $expected);
+    }    
+     
+    /**
+     * Tests for the removal of jibberish
+     */
+    public function testRemoveEmpty(){
+        $result = Scrub::html('<div><p>Test String</p><span></span><p>&nbsp;</p></div>');
+        $expected = '<div><p>Test String</p></div>';
+        $this->assertEqual($result, $expected);
+    }       
+    
+    /**
      * @todo Test against ha.ckers.org xxs sheetsheet
      */
     public function testXXS(){
