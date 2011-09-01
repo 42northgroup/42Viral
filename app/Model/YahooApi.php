ada58f3ad437c437f347c485728aa661c39221d2
Zubin Khavarian <zubin.khavarian@gmail.com>
2011-09-01 09:29:47 -0500

    implemented Yahoo API data source

---
+++ app/Model/YahooApi.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * PHP 5.3
+ *
+ * 42Viral(tm) : The 42Viral Project (http://42viral.org)
+ * Copyright 2009-2011, 42 North Group Inc. (http://42northgroup.com)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright 2009-2011, 42 North Group Inc. (http://42northgroup.com)
+ * @link          http://42viral.org 42Viral(tm)
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+
+App::uses('YahooApiAbstract', 'Model');
+
+/**
+ * @author Zubin Khavarian <zubin.khavarian@42viral.com>
+ * @package App
+ * @subpackage App.core
+ */
+
+class YahooApi extends YahooApiAbstract {}
+
+?>
