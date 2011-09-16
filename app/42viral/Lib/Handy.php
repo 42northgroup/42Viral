0d5deb51920019fa808530b10ec49aa5461aff9e
Jason Snider <jsnider77@gmail.com>
2011-09-16 11:06:30 -0700

    Added time and date tools to handy

--- app/42viral/Lib/Handy.php
+++ app/42viral/Lib/Handy.php
@@ -41,5 +41,46 @@ class Handy
 
         return $arr;
     }
+    
+    /**
+     * Returns a formatted date string, given either a UNIX timestamp or a valid strtotime() date string.
+     * This function also accepts a time string and a format string as first and second parameters.
+     * In that case this function behaves as a wrapper for TimeHelper::i18nFormat()
+     *
+     * @param string $format date format string (or a DateTime string)
+     * @param string $date Datetime string (or a date format string)
+     * @param boolean $invalid flag to ignore results of fromString == false
+     * @param integer $userOffset User's offset from GMT (in hours)
+     * @return string Formatted date string
+     */
+    public static function date($time = null, $format = 'm/d/y h:i a', $fail = 'Not Set') {
+        $time = self::validTime($time);
+        
+        if(is_null($time)){
+            return $fail;
+        }else{
+            return date($format, $time);
+        }
+    }
+   
+    /**
+     * Prevets bad timestamps from defaulting to the epoch
+     * @param type $timestamp
+     * @return type 
+     * 
+     * @see http://stackoverflow.com/questions/2224097/prevent-php-date-from-defaulting-to-12-31-1969
+     */
+    public static function validTime($time, $returnNull = true)
+    { 
+        
+        $returnNull ? null : '0000-00-00 00:00:00';
+        
+        $time = strtotime($time);
+        
+        $date = ($time === false) ? $returnNull : $time;
+        
+        return $date;
+        
+    }
 
 }
