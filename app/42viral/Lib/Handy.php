726f1259957f162d242a13f7c346a47211d71013
Jason Snider <jsnider77@gmail.com>
2011-09-17 04:43:32 -0700

    Updated Handy methods
    - Updated random() to allow special chars
    - Added truncate()

--- app/42viral/Lib/Handy.php
+++ app/42viral/Lib/Handy.php
@@ -82,14 +82,14 @@ class Handy
     }
     
     /**
-     * Generates a random string
+     * Generates a random string of a specified length and character set
      * @param integer $length The length of the string
-     * @param boolean $upper Add 
-     * @param boolean $lower
-     * @param boolean $num
+     * @param boolean $upper Add the A-Z character set
+     * @param boolean $lower Add the a-z character set
+     * @param boolean $numeric Add the numeric character set
      * @return string 
      */
-    public static function random($length = 5, $upper = false, $lower = true, $numeric = true){
+    public static function random($length = 5, $upper = false, $lower = true, $numeric = true, $special = false){
         
         $characters = '';
         $string = ''; 
@@ -105,6 +105,10 @@ class Handy
         if($upper){
             $characters .= 'ABCDEFGHIKLMNOPQRSTUVWXYZ';
         }
+        
+        if($special){
+            $characters .= '!@#$%^&*()-_=+;:\'"<>,./?\\`~';
+        }        
            
         $size = strlen( $characters );
         for( $i = 0; $i < $length; $i++ ) {
@@ -112,6 +116,119 @@ class Handy
         }
         
         return $string;
+    }
+    
+    /**
+     * Truncates text.
+     *
+     * Cuts a string to the length of $length and replaces the last characters
+     * with the ending if the text is longer than length.
+     *
+     * ### Options:
+     *
+     * - `ending` Will be used as Ending and appended to the trimmed string
+     * - `exact` If false, $text will not be cut mid-word
+     * - `html` If true, HTML tags would be handled correctly
+     *
+     * @param string $text String to truncate.
+     * @param integer $length Length of returned string, including ellipsis.
+     * @param array $options An array of html attributes and options.
+     * @return string Trimmed string.
+     * @link http://book.cakephp.org/view/1469/Text#truncate-1625
+     */
+    public function truncate($text, $length = 100, $options = array()) {
+        $default = array(
+            'ending' => '...', 'exact' => true, 'html' => false
+        );
+        $options = array_merge($default, $options);
+        extract($options);
+
+        if (!function_exists('mb_strlen')) {
+            class_exists('Multibyte');
+        }
+
+        if ($html) {
+            if (mb_strlen(preg_replace('/<.*?>/', '', $text)) <= $length) {
+                return $text;
+            }
+            $totalLength = mb_strlen(strip_tags($ending));
+            $openTags = array();
+            $truncate = '';
+
+            preg_match_all('/(<\/?([\w+]+)[^>]*>)?([^<>]*)/', $text, $tags, PREG_SET_ORDER);
+            foreach ($tags as $tag) {
+                if (!preg_match('/img|br|input|hr|area|base|basefont|col|frame|isindex|link|meta|param/s', $tag[2])) {
+                    if (preg_match('/<[\w]+[^>]*>/s', $tag[0])) {
+                        array_unshift($openTags, $tag[2]);
+                    } else if (preg_match('/<\/([\w]+)[^>]*>/s', $tag[0], $closeTag)) {
+                        $pos = array_search($closeTag[1], $openTags);
+                        if ($pos !== false) {
+                            array_splice($openTags, $pos, 1);
+                        }
+                    }
+                }
+                $truncate .= $tag[1];
+
+                $contentLength = 
+                    mb_strlen(preg_replace('/&[0-9a-z]{2,8};|&#[0-9]{1,7};|&#x[0-9a-f]{1,6};/i', ' ', $tag[3]));
+                if ($contentLength + $totalLength > $length) {
+                    $left = $length - $totalLength;
+                    $entitiesLength = 0;
+                    if (preg_match_all('/&[0-9a-z]{2,8};|&#[0-9]{1,7};|&#x[0-9a-f]{1,6};/i', $tag[3], $entities, 
+                                PREG_OFFSET_CAPTURE)) {
+                        foreach ($entities[0] as $entity) {
+                            if ($entity[1] + 1 - $entitiesLength <= $left) {
+                                $left--;
+                                $entitiesLength += mb_strlen($entity[0]);
+                            } else {
+                                break;
+                            }
+                        }
+                    }
+
+                    $truncate .= mb_substr($tag[3], 0, $left + $entitiesLength);
+                    break;
+                } else {
+                    $truncate .= $tag[3];
+                    $totalLength += $contentLength;
+                }
+                if ($totalLength >= $length) {
+                    break;
+                }
+            }
+        } else {
+            if (mb_strlen($text) <= $length) {
+                return $text;
+            } else {
+                $truncate = mb_substr($text, 0, $length - mb_strlen($ending));
+            }
+        }
+        if (!$exact) {
+            $spacepos = mb_strrpos($truncate, ' ');
+            if (isset($spacepos)) {
+                if ($html) {
+                    $bits = mb_substr($truncate, $spacepos);
+                    preg_match_all('/<\/([a-z]+)>/', $bits, $droppedTags, PREG_SET_ORDER);
+                    if (!empty($droppedTags)) {
+                        foreach ($droppedTags as $closingTag) {
+                            if (!in_array($closingTag[1], $openTags)) {
+                                array_unshift($openTags, $closingTag[1]);
+                            }
+                        }
+                    }
+                }
+                $truncate = mb_substr($truncate, 0, $spacepos);
+            }
+        }
+        $truncate .= $ending;
+
+        if ($html) {
+            foreach ($openTags as $tag) {
+                $truncate .= '</' . $tag . '>';
+            }
+        }
+
+        return $truncate;
     }
 
 }
