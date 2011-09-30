8b51cfc4c8f04ca2421a41e79f4940ae62bc39fa
Jason Snider <jsnider77@gmail.com>
2011-09-30 14:15:20 -0700

    Invite logic, proof of concept

---
+++ app/42viral/Model/Abstract/InviteAbstract.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * PHP 5.3
+ * 
+ * 42Viral(tm) : The 42Viral Project (http://42viral.org)
+ * Copyright 2009-2011, 42 North Group Inc. (http://42northgroup.com)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright 2009-2011, 42 North Group Inc. (http://42northgroup.com)
+ * @link          http://42viral.org 42Viral(tm)
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+App::uses('AppModel', 'Model');
+
+/**
+ * Functionality for sending out invites, this was inspired by a need for private invites,
+ * @author Jason D Snider <jason.snider@42viral.org>
+ */
+class InviteAbstract extends AppModel
+{
+    /**
+     * 
+     * @var string
+     * @access public
+     */
+    public $name = 'Invite';
+    
+    /**
+     * 
+     * @var string
+     * @access public
+     */
+    public $useTable = 'invites';
+}
