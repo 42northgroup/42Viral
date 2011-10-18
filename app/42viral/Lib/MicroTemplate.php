5fe86b87dd5709929ce0bb2b0b70da1eaa7f0197
Zubin Khavarian <zubin.khavarian@gmail.com>
2011-10-18 15:27:14 -0500

    Integrated Micro Templating with Mustache (PHP version) capabilities to include loop constructs

--- app/42viral/Lib/MicroTemplate.php
+++ app/42viral/Lib/MicroTemplate.php
@@ -13,101 +13,24 @@
  * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
 
+App::uses('Mustache', 'Lib');
+
 /**
  * @package Lib
- *** @author Zubin Khavarian <zubin.khavarian@42viral.org>
+ * @author Zubin Khavarian <zubin.khavarian@42viral.org>
  */
 class MicroTemplate extends Object
 {
+    private static $__MI = null;
+    private static $__standardTemplateObjects = array();
 
     private static function __init()
     {
-        self::$__serverAddress = Configure::read('Domain.url');
-        //self::$__standardTemplateTags = Configure::read('TemplateObjects.standard');
-    }
+        self::$__MI = new Mustache();
 
-/**
- * Stores a list of standard template tags to be substituted for all strings if they contain the template tag and is
- * set using the 'TemplateObjects.standard' configuration setting.
- *
- * @access private
- * @var array
- */
-    private static $__standardTemplateTags = array();
-    
-    
-/**
- * Store the server address for substitution in template string which is set using the
- * 'Domain.url' configuration setting.
- *
- * @access private
- * @var string
- */
-    private static $__serverAddress = '';
-
-
-/**
- * Apply the standard objects to the standard template tags
- *
- *** @author Zubin Khavarian <zubin.khavarian@42viral.com>
- * @access private
- * @param string $baseString
- * @return string
- */
-    private static function __applyStandardTemplateObject($baseString)
-    {
-        $htmlString = $baseString;
-
-        if(!empty(self::$__standardTemplateTags)) {
-            foreach(self::$__standardTemplateTags as $key => $value) {
-                $replaceString = $value;
-                $templateString = '#{' . $key . '}';
-                $htmlString = str_replace($templateString, $replaceString, $htmlString);
-            }
-        }
-
-        return $htmlString;
-    }
-
-
-
-/**
- * Apply the server address to the #{server_address} template tag
- *
- *** @author Zubin Khavarian <zubin.khavarian@42viral.com>
- * @access private
- * @param string $baseString
- * @return string
- */
-    private static function __applyServerAddress($baseString)
-    {
-        $htmlString = $baseString;
-
-        if(!empty(self::$__serverAddress)) {
-            $htmlString = str_replace('#{_server_address}', self::$__serverAddress, $htmlString);
-        }
-
-        return $htmlString;
-    }
-
-
-/**
- * Prevent display of missing tag data by cleaning up any remaining tag pseudo-variables
- * 
- *** @author Zubin Khavarian <zubin.khavarian@42viral.com>
- * @access private
- * @param type $baseString
- * @return string
- */
-    private static function __cleanUp($baseString)
-    {
-        $htmlString = $baseString;
-
-        if(preg_match('/\#\{.*\}/U', $htmlString) > 0) {
-            $htmlString = preg_replace('/\#\{.*\}/U', '', $htmlString);
-        }
-
-        return $htmlString;
+        self::$__standardTemplateObjects = array(
+            '_server_address' => Configure::read('Domain.url')
+        );
     }
 
 
@@ -115,7 +38,6 @@ class MicroTemplate extends Object
  * Expands the objects into the $baseString by using the mappingParams array
  *
  * @access public
- *** @author Zubin Khavarian <zubin.khavarian@42viral.com>
  * @param string $baseString the base template string to which the objects need to be applied
  * @param array $templateObjects array of objects with which the $baseString needs to be populated
  * @return string the generated string after expanding objects into the base template string
@@ -123,21 +45,10 @@ class MicroTemplate extends Object
     public static function applyTemplate($baseString, $templateObjects)
     {
         self::__init();
-        
-        $htmlString = self::__applyStandardTemplateObject($baseString);
 
+        $templateObjects = array_merge($templateObjects, self::$__standardTemplateObjects);
         $flattenedTemplateObject = Set::flatten($templateObjects);
-
-        if(!empty($flattenedTemplateObject)) {
-            foreach($flattenedTemplateObject as $key => $value) {
-                $replaceString = $value;
-                $templateString = '#{' . $key . '}';
-                $htmlString = str_replace($templateString, $replaceString, $htmlString);
-            }
-        }
-
-        $htmlString = self::__applyServerAddress($htmlString);
-        $htmlString = self::__cleanUp($htmlString);
+        $htmlString = self::$__MI->render($baseString, $templateObjects);
 
         return $htmlString;
     }
