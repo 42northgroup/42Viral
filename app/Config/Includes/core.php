ad174b0671a6478ec55360e512aae42ec03eab6b
Jason Snider <jsnider77@gmail.com>
2011-10-19 08:48:42 -0500

    Set config files and XML back to the desired default state

--- app/Config/Includes/core.php
+++ app/Config/Includes/core.php
@@ -1,3 +1,4 @@
 <?php
 Configure::write('debug', 2);
 Configure::write('Security.level', 'medium');
+Configure::write('Session', array('defaults' => 'php'));
--- app/Config/Includes/site.php
+++ app/Config/Includes/site.php
@@ -2,11 +2,11 @@
 Configure::write('Theme.set', 'Default');
 Configure::write('Theme.HomePage.title', 'Default');
 Configure::write('Domain.scheme', 'http');
-Configure::write('Domain.host', 'loc.build.42viral.org');
-Configure::write('Email.from', 'app@42viral.org');
-Configure::write('Email.replyTo', 'support@42viral.org');
+Configure::write('Domain.host', '');
+Configure::write('Email.from', '');
+Configure::write('Email.replyTo', '');
 Configure::write('ShortURL.scheme', 'http');
-Configure::write('ShortURL.host', 'loc.42viral.us');
+Configure::write('ShortURL.host', '');
 Configure::write('ShortURL.Pointer.page', 'a');
 Configure::write('ShortURL.Pointer.blog', 'b');
 Configure::write('ShortURL.Pointer.post', 'c');
