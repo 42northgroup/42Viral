695cc1d7ae3f388fba76758ca1d19d989fb30319
Jason Snider <jsnider77@gmail.com>
2012-02-22 15:33:17 -0600

    Added testing for the SEOBehavior

--- app/Test/Fixture/ArticleFixture.php
+++ app/Test/Fixture/ArticleFixture.php
@@ -1,28 +1,8 @@
 <?php
-/**
- * Short description for file.
- *
- * PHP versions 4 and 5
- *
- * CakePHP(tm) Tests <http://book.cakephp.org/view/1196/Testing>
- * Copyright 2005-2010, Cake Software Foundation, Inc. (http://cakefoundation.org)
- *
- *  Licensed under The Open Group Test Suite License
- *  Redistributions of files must retain the above copyright notice.
- *
- * @copyright     Copyright 2005-2010, Cake Software Foundation, Inc. (http://cakefoundation.org)
- * @link          http://book.cakephp.org/view/1196/Testing CakePHP(tm) Tests
- * @package       cake
- * @subpackage    cake.tests.fixtures
- * @since         CakePHP(tm) v 1.2.0.4667
- * @license       http://www.opensource.org/licenses/opengroup.php The Open Group Test Suite License
- */
 
 /**
- * Short description for class.
- *
- * @package       cake
- * @subpackage    cake.tests.fixtures
+ * @package       Test
+ * @subpackage    Test.fixtures
  */
 class ArticleFixture extends CakeTestFixture {
 
@@ -41,62 +21,43 @@ class ArticleFixture extends CakeTestFixture {
  * @access public
  */
 	var $fields = array(
-		'id' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 36, 'key' => 'primary', 'collate' => 'latin1_swedish_ci', 'comment' => '', 'charset' => 'latin1'),
-		'title' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 500, 'collate' => 'latin1_swedish_ci', 'comment' => '', 'charset' => 'latin1'),
-		'slug' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 500, 'key' => 'unique', 'collate' => 'latin1_swedish_ci', 'comment' => '', 'charset' => 'latin1'),
-		'base_slug' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 500, 'collate' => 'latin1_swedish_ci', 'comment' => '', 'charset' => 'latin1'),
-		'short_cut' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 6, 'key' => 'unique', 'collate' => 'latin1_swedish_ci', 'comment' => '', 'charset' => 'latin1'),
-		'body' => array('type' => 'text', 'null' => true, 'default' => NULL, 'collate' => 'latin1_swedish_ci', 'comment' => '', 'charset' => 'latin1'),
-		'tease' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 140, 'collate' => 'latin1_swedish_ci', 'comment' => '', 'charset' => 'latin1'),
-		'description' => array('type' => 'text', 'null' => true, 'default' => NULL, 'collate' => 'latin1_swedish_ci', 'comment' => 'Meta description tag', 'charset' => 'latin1'),
-		'keywords' => array('type' => 'text', 'null' => true, 'default' => NULL, 'collate' => 'latin1_swedish_ci', 'comment' => 'Meta keyword tag', 'charset' => 'latin1'),
-		'canonical' => array('type' => 'text', 'null' => true, 'default' => NULL, 'collate' => 'latin1_swedish_ci', 'comment' => 'Your preferred version of a URL for the disambiguation of like content ', 'charset' => 'latin1'),
-		'syntax' => array('type' => 'string', 'null' => false, 'default' => 'html', 'length' => 45, 'collate' => 'latin1_swedish_ci', 'comment' => '', 'charset' => 'latin1'),
-		'status' => array('type' => 'string', 'null' => false, 'default' => 'draft', 'length' => 45, 'collate' => 'latin1_swedish_ci', 'comment' => '', 'charset' => 'latin1'),
-		'custom_file' => array('type' => 'string', 'null' => true, 'default' => NULL, 'collate' => 'latin1_swedish_ci', 'comment' => '', 'charset' => 'latin1'),
-		'object_type' => array('type' => 'string', 'null' => false, 'default' => 'page', 'length' => 45, 'collate' => 'latin1_swedish_ci', 'comment' => '', 'charset' => 'latin1'),
-		'parent_content_id' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 36, 'collate' => 'latin1_swedish_ci', 'comment' => '', 'charset' => 'latin1'),
-		'created_person_id' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 36, 'collate' => 'latin1_swedish_ci', 'comment' => '', 'charset' => 'latin1'),
-		'created' => array('type' => 'datetime', 'null' => false, 'default' => NULL, 'collate' => NULL, 'comment' => ''),
-		'modified_person_id' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 36, 'collate' => 'latin1_swedish_ci', 'comment' => '', 'charset' => 'latin1'),
-		'modified' => array('type' => 'datetime', 'null' => false, 'default' => NULL, 'collate' => NULL, 'comment' => ''),
-		'deleted' => array('type' => 'datetime', 'null' => true, 'default' => NULL, 'collate' => NULL, 'comment' => ''),
-		'deleted_person_id' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 36, 'collate' => 'latin1_swedish_ci', 'comment' => '', 'charset' => 'latin1')
+            'id' => array('type' => 'integer', 'key' => 'primary'),
+            'user_id' => array('type' => 'integer', 'null' => false),
+            'name' => array('type' => 'string', 'null' => false),
+            'base_slug' => array('type' => 'string', 'null' => false),
+            'slug' => array('type' => 'string', 'null' => false),
+            'body' => 'text',
+            'published' => array('type' => 'string', 'length' => 1, 'default' => 'N'),
+            'created' => 'datetime',
+            'updated' => 'datetime'
 	);
 
-/**
- * records property
- *
- * @var array
- * @access public
- */
-        /*
-	var $records = array(
-		array('user_id' => 1, 
-                      'title' => 'First Article', 
-                      'base_slug'=>'first-article',
-                      'slug'=>'first-article',
-                      'body' => 'First Article Body', 
-                      'published' => 'Y', 'created' => '2007-03-18 10:39:23', 
-                      'updated' => '2007-03-18 10:41:31'),
-            
-		array('user_id' => 3, 
-                      'title' => 'Second Article', 
-                      'body' => 'Second Article Body',
-                      'base_slug'=>'second-article',
-                      'slug'=>'second-article',                    
-                      'published' => 'Y', 
-                      'created' => '2007-03-18 10:41:23', 
-                      'updated' => '2007-03-18 10:43:31'),
-            
-		array('user_id' => 1, 
-                      'title' => 'Third Article', 
-                      'body' => 'Third Article Body', 
-                      'base_slug'=>'third-article',
-                      'slug'=>'third-article',                       
-                      'published' => 'Y', 
-                      'created' => '2007-03-18 10:43:23', 
-                      'updated' => '2007-03-18 10:45:31')
-	);
-         */
+    /**
+     * records property
+     *
+     * @var array
+     * @access public
+     */
+    var $records = array(
+        array(
+            'user_id' => 1, 
+            'name' => 'Test Article', 
+            'base_slug' => 'test-article',
+            'slug' => 'test-article',
+            'body' => 'First Article Body', 
+            'published' => 'Y', 
+            'created' => '2007-03-18 10:39:23', 
+            'updated' => '2007-03-18 10:41:31'
+        ),
+        array(
+            'user_id' => 1, 
+            'name' => 'Test Article', 
+            'base_slug' => 'test-article',
+            'slug' => 'test-article-1',
+            'body' => 'Second Article Body', 
+            'published' => 'Y', 
+            'created' => '2007-03-18 10:55:23', 
+            'updated' => '2007-03-18 10:56:31'
+        ),
+    );
 }
