53737da8d6e4aff065afb89563a296a59feb86c9
Jason Snider <jsnider77@gmail.com>
2012-03-16 09:03:59 -0500

    Setup fixes

--- Config/application.php
+++ Config/application.php
@@ -1,8 +1,2 @@
 <?php
-Configure::write('Comment.engine', 'native');
-Configure::write('Oauth.password', 'password');
-Configure::write('Session', array('defaults' => 'database'));
-Configure::write('ShortURL.Pointer.blog', 'a');
-Configure::write('ShortURL.Pointer.page', 'b');
-Configure::write('ShortURL.Pointer.post', 'c');
-Configure::write('Theme.set', 'Default');
+
