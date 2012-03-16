e8c97638f1b8577cc625a3eb0bdae927c7675230
Jason Snider <jsnider77@gmail.com>
2012-03-16 08:51:20 -0500

    Fixing setup errors

--- Config/Schema/schema.php
+++ Config/Schema/schema.php
@@ -179,6 +179,7 @@ class AppSchema extends CakeSchema {
 	public $configurations = array(
 		'id' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 100, 'key' => 'primary', 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
 		'value' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 100, 'collate' => 'latin1_swedish_ci', 'comment' => 'The value of a configuration varaible', 'charset' => 'latin1'),
+		'type' => array('type' => 'string', 'null' => true, 'default' => 'string', 'length' => 100, 'collate' => 'latin1_swedish_ci', 'comment' => 'We should add quotes if it is a string type', 'charset' => 'latin1'),
 		'created_person_id' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 36, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
 		'created' => array('type' => 'datetime', 'null' => false, 'default' => NULL),
 		'modified_person_id' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 36, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
