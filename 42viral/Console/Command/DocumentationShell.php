2309def579ad350f62c266adbf705518969f1adf
Zubin Khavarian <zubin.khavarian@gmail.com>
2012-04-13 16:24:29 -0500

    Documentation shell recursive directory traversal and markdown converter (not complete)

--- 42viral/Console/Command/DocumentationShell.php
+++ 42viral/Console/Command/DocumentationShell.php
@@ -13,22 +13,101 @@
  * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
 
+App::uses('Utility', 'Lib');
+
 /**
+ *
+ *
+ * 
  */
 class DocumentationShell extends AppShell
 {
+    private $__documentationPath = null;
+
  
     /**
-     * @return void
+     * 
+     * 
      * @access public
+     * @return void
      */
     public function main()
     {
-        $documentationPath = ROOT . DS . APP_DIR . DS . '42viral' . DS . 'DocumentationMarkdown';
-        print_r($this->__fetchFolders($documentationPath));
+        $this->__documentationPath = ROOT . DS . APP_DIR . DS . '42viral' . DS . 'Documentation/';
+        $files = $this->__traverseFilterDocDir();
+
+        foreach($files as $file) {
+            $html = $this->__convertMarkdownToHtml($file['full']);
+
+            //[TODO] Save html to _build folder, mirroring the folder hierarchy of the markdown version
+        }
     }
-    
-    private function __fetchFolders($path){
-        return scandir($path);
+
+    /**
+     * Convert an individual markdown file to HTML
+     *
+     * @access private
+     * @param array $file
+     * @return string
+     */
+    private function __convertMarkdownToHtml($file)
+    {
+        $fileHandle = fopen($file, 'r');
+        $fileContent = fread($fileHandle, filesize($file));
+        $fileHtmlContent = Utility::markdown($fileContent);
+        fclose($fileHandle);
+
+        return $fileHtmlContent;
+    }
+
+    /**
+     * Takes the documentation path and traverses through the folder looking for markdown files and hierarchy
+     *
+     * @access private
+     * @param string $path
+     * @return array
+     */
+    private function __traverseFilterDocDir()
+    {
+        $path = $this->__documentationPath;
+        $files = array();
+        $allowedExtension = array('md');
+        
+        $fileSPLObjects =  new RecursiveIteratorIterator(
+            new RecursiveDirectoryIterator($path),
+            RecursiveIteratorIterator::CHILD_FIRST
+        );
+        
+        try {
+            foreach($fileSPLObjects as $fullFileName => $Item) {
+                if($Item->isFile()) {
+                    $fileName = $Item->getFilename();
+                    $path = $Item->getPath();
+
+                    //Skip files without the allowed extensions
+                    $pos_dot = strrpos($fileName, "."); // find '.'
+                    $fileExtension = ($pos_dot !== false) ? substr($fileName, $pos_dot+1) : null;
+                    if(!in_array($fileExtension, $allowedExtension)) {
+                        continue;
+                    }
+
+                    //Skip files in the _build folder, since that's where the generated doc is placed
+                    if(strpos($path, '_build')) {
+                        continue;
+                    }
+
+                    $files[] = array(
+                        'file' => $fileName,
+                        'path' => $Item->getPath(),
+                        'full' => $fullFileName
+                    );
+                }
+            }
+        }
+        catch (UnexpectedValueException $e) {
+            printf("Directory [%s] contained a directory we can not recurse into", $path);
+        }
+        
+        return $files;
     }
 }
