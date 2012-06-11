b5b53e75daf47e7e4f5a4436162c4fd0570dbd70
Jason Snider <jsnider77@gmail.com>
2012-06-11 16:59:43 -0500

    Set core debug back to level 2

--- Config/core.php
+++ Config/core.php
@@ -28,6 +28,7 @@
  * Development Mode:
  * 	1: Errors and warnings shown, model caches refreshed, flash messages halted.
  * 	2: As in 1, but also with full debug messages and SQL output.
+
  *
  * In production mode, flash messages redirect after a time interval.
  * In development mode, you need to click the flash message to continue.
