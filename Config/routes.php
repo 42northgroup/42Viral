555f8f5a832ce025d896db9a5b21481abd7fa0e8
Jason Snider <jsnider77@gmail.com>
2012-08-31 09:28:10 -0500

    Moved content related routes and consoles to the COntent plugin

--- Config/routes.php
+++ Config/routes.php
@@ -20,22 +20,6 @@
  * @since         CakePHP(tm) v 0.2.9
  * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
-
-Router::connect('/', array('controller' => 'pages', 'action' => 'display', 'home'));
-
-//A convenience route for accessing admin sections
-Router::connect('/admin', array('prefix'=>'admin', 'controller' => 'admin', 'action' => 'index'));
-
-//Extra pretty URLs for content actions
-Router::connect('/page', array('controller' => 'pages', 'action' => 'index'));
-Router::connect('/page/:slug', array('controller' => 'pages', 'action' => 'view'), array('pass' => array('slug')));
-
-Router::connect('/blog', array('controller' => 'blogs', 'action' => 'index'));
-Router::connect('/blog/:slug', array('controller' => 'blogs', 'action' => 'view'), array('pass' => array('slug')));
-
-Router::connect('/post', array('controller' => 'posts', 'action' => 'index'));
-Router::connect('/post/:slug', array('controller' => 'posts', 'action' => 'view'), array('pass' => array('slug')));
-
 //a convenience route for accesing a profile
 Router::connect('/p/:username',
         array('controller' => 'profiles', 'action' => 'view'), array('pass' => array('username')));
