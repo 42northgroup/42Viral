89ca96ad9afe3c2c0063f06dd887e20b891d3aec
Jason Snider <jsnider77@gmail.com>
2012-09-02 16:43:47 -0500

    Moving pluginified assets

--- Config/routes.php
+++ Config/routes.php
@@ -20,6 +20,18 @@
  * @since         CakePHP(tm) v 0.2.9
  * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
+
+/**
+ * Here, we are connecting '/' (base path) to controller called 'Pages',
+ * its action called 'display', and we pass a param to select the view file
+ * to use (in this case, /app/View/Pages/home.ctp)...
+ */
+Router::connect('/', array( 'controller' => 'profiles', 'action' => 'index'));
+/**
+ * ...and connect the rest of 'Pages' controller's urls.
+ */
+Router::connect('/pages/*', array('controller' => 'pages', 'action' => 'display'));
+
 //a convenience route for accesing a profile
 Router::connect('/p/:username',
         array('controller' => 'profiles', 'action' => 'view'), array('pass' => array('username')));
